56ee8b67ec72adfa8daf5ebdabb09bf2e5c3e3fb
Birendra <birendra@rudrainnovative.in>
2023-07-20 19:09:35 +0530

    first

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production:false,
+  firebase: {
+    projectId: 'student-management-syste-fb40c',
+    appId: '1:302715314396:web:3a8a1d7c39e1ef31955419',
+    storageBucket: 'student-management-syste-fb40c.appspot.com',
+    apiKey: 'AIzaSyAV7jFC01Pr4SYQN6rU4y8getS3TL_qlSg',
+    authDomain: 'student-management-syste-fb40c.firebaseapp.com',
+    messagingSenderId: '302715314396',
+  }
+};
